d1e84eae8fc132b7c7bc4c68507ec76c40b5c32c
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-04 21:33:08 +0300

    Change#003 Add set

--- Python/Set.py
+++ Python/Set.py
@@ -1,2 +1,7 @@
-my_set = set('hi there!')
-print(my_set)
+list_data_one = [1, 1, 2, 3, 5, 8, 13, 21, 7, 5, 100]
+list_data_two = [11, 1, 21, 31, 15, 8, 13, 21, 7, 15, 101]
+
+print(list(set(list_data_one) & set(list_data_two))) # in first and in second &(and)
+print(list(set(list_data_one) | set(list_data_two))) # in first or in second |(or)
+print(list(set(list_data_one) - set(list_data_two))) # in first and not in second -(minus)
+print(list(set(list_data_one) ^ set(list_data_two))) # in first and not in second + in second and not in first
