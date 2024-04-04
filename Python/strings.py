e119815b56214a5fc0aa079860d0a43b2e89acf7
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-04 21:39:52 +0300

    Change#004 add strings

---
+++ Python/strings.py
@@ -0,0 +1,11 @@
+my_string = 'hello my liTTle friends!'
+
+print(my_string) # hello my liTTle friends!
+print(my_string.upper()) # HELLO MY LITTLE FRIENDS!
+print(my_string.lower()) # hello my little friends!
+print(my_string.startswith('hello')) # True
+print(my_string.startswith('hi')) # False
+print(my_string.endswith('!')) # True
+print(my_string.endswith('.')) # False
+print(my_string.title()) # Hello My Little Friends!
+print(my_string.count('e')) # 3
