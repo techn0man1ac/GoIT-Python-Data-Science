081cb23a681c112e8d87890f2edf3bf79ab9a38b
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-07 13:59:26 +0300

    Change#006 Add some AI's

---
+++ Python/AI_v00.py
@@ -0,0 +1,8 @@
+# Введення (отримання даних)
+InputName = input("Input Name: ")
+
+# Перетворення (обробка даних)
+meaasgeAI = f"Hello, {InputName}!"
+
+# Виведення (виведення даних)
+print(meaasgeAI)
