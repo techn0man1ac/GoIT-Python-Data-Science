c57b0cef1ca95fcba855f0b518b8805741821550
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-07 16:54:51 +0300

    Change#008 Add some math stuffs

--- Python/Math.py
+++ Python/Math.py
@@ -7,3 +7,12 @@ complex_number = 3.3 + 2j
 print(int_number)
 print(float_number)
 print(complex_number)
+
+# формула відстані Евкліда
+x1 = 12
+y1 = 14
+x2 = 34
+y2 = 54
+d = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+print(d)
