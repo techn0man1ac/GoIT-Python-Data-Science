2e0beace340231bc252cb92be171fb87c76e8534
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-08 15:56:46 +0300

    Change#011 Словники

--- Python/dict.py
+++ Python/dict.py
@@ -1,6 +1,6 @@
 # Словники
 my_dict = {"name": "Serhii", "age": 32, "city": "Lutsk"}
 print(f"my_dict: {my_dict}") 
-my_dict["age"] = 33  # Змінює вік на 26
-my_dict["email"] = "hwman1991@example.com"  # Додає нову пару ключ-значення
+my_dict["age"] = 33  # Змінює вік на 336
+my_dict["email"] = "qwertyyyy@example.com"  # Додає нову пару ключ-значення
 print(f"my_dict(new): {my_dict}") 
