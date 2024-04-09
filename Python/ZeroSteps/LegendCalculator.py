421c3fbe46e76bca26183f7af5b3c905dfdf4fe4
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-09 10:44:29 +0300

    Change#013 Legend calculator in one string

--- Python/ZeroSteps/LegendCalculator.py
+++ Python/ZeroSteps/LegendCalculator.py
@@ -1,1 +1,22 @@
-value_x = input("Legend calculator: ") / print(eval(value_x))
+value0 = float(input("Value 1 = "))
+operation = input("Operation(+, -, *, **, /, //, %): ")
+value1 = float(input("Value 2 = "))
+
+output = ''
+
+if operation == '+':
+   output = value0 + value1
+elif operation == '-':
+   output = value0 - value1
+elif operation == '*':
+   output = value0 * value1
+elif operation == '**':
+   output = value0 ** value1
+elif operation == '/':
+   output = value0 / value1
+elif operation == '//':
+   output = value0 // value1
+elif operation == '%':
+   output = value0 % value1
+
+print(output)
