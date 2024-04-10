6673f9ee46476a57331e8bc35102030786aab377
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-10 22:03:04 +0300

    Change#016 Add identical check

--- Python/ZeroSteps/valueIndenticalCheck.py
+++ Python/ZeroSteps/valueIndenticalCheck.py
@@ -7,6 +7,6 @@ print(sentence_one, sentence_two, sep='\n')
 print(f'Are sentence equal? {sentence_one == sentence_two}')
 print(f'Are sentence identical? {sentence_one is sentence_two}')
 
-# Перевірка місця збереження у памяті
+# Перевірка місця збереження у пам'яті
 print(f'Path of sentence in memory {id(sentence_one)}')
 print(f'Path of sentence in memory {id(sentence_two)}')
