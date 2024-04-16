b0767ae37c0f6aa690c9d989ad975be35c726f1f
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-16 14:12:44 +0300

    Change#049 add legendCalculator V0.2

--- Python/StepTwo/legendCalculatorV0_2.py
+++ Python/StepTwo/legendCalculatorV0_2.py
@@ -6,15 +6,8 @@ value0 = 0.0
 operation =''
 value1 = 0.0
 
-try:
-    value0 = float(input("Value 1 = "))
-    operation = input("Operation(+, -, *, **, /, //, %): ")
-    value1 = float(input("Value 2 = "))
-
-except ValueError:
-    print("Value 1/Value 2 is not a number")
-else:
-    #print(f'op. == "/" {operation == "/"} op. == "//" {operation == "//"} val.1 == 0 {value1 == 0} ')
+def mathMagic():
+#print(f'op. == "/" {operation == "/"} op. == "//" {operation == "//"} val.1 == 0 {value1 == 0} ')
 
     if (operation == "/" or operation == "//") and value1 == 0:
         output = 'Error, divide by zero'
@@ -23,7 +16,7 @@ else:
             case '+':
                 output = value0 + value1
             case '-':
-                output = value0 - value1
+               output = value0 - value1
             case '*':
                 output = value0 * value1
             case '**':
@@ -35,9 +28,18 @@ else:
             case '%':
                 output = value0 % value1
             case _:
-                output = 'Error, unknown operation'
+                output = 'Error, unknown operation' 
+    return output
 
-    print(output)
+try: # make exeption - protect wrong user input
+    value0 = float(input("Value 1 = "))
+    operation = input("Operation(+, -, *, **, /, //, %): ")
+    value1 = float(input("Value 2 = "))
 
+except ValueError:
+    print("Value 1/Value 2 is not a number")
+else:
+    
+    print(mathMagic())
 finally:
     print("End")
