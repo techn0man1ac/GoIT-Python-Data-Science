1ed493f19e6f296b0cceec69bc39b4e13121463f
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-16 10:55:41 +0300

    Change#044 list to Magic Ball

--- Python/StepTwo/magicBall.py
+++ Python/StepTwo/magicBall.py
@@ -1,5 +1,13 @@
 import random
 from datetime import datetime
 
+responce = ["Many doubts,", "So it will be,", "Ask later", "Yes", 
+            "I can't answer now", "Chances are good", 
+            "Ask the question more precisely", "Definitely", 
+            "Now unknown,", "No doubts, many doubts,", 
+            "Definitely yes", "Good chances", "Repeat the question," , 
+            "Maybe", "Will come true,", "A bad outlook", 
+            "The stars say no", "It should be so"]
+
 random.seed = input("Ask your question: ") + str(datetime.now())
-print(random.choice(["Many doubts,", "So it will be,", "Ask later", "Yes", "I can't answer now", "Chances are good", "Ask the question more precisely", "Definitely", "Now unknown,", "No doubts, many doubts,", "Definitely yes", "Good chances", "Repeat the question," , "Maybe", "Will come true,", "A bad outlook", "The stars say no", "It should be so"]))
+print(random.choice(responce))
