52e4b92bdd90b6a771233f702b6e2fcf935acac4
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-24 16:36:34 +0300

    Change#064 Fixed JSON generating

--- MyProjects/ESP_To_Python/SourceCodes/WiFi_to_GET/WiFi_to_GET.ino
+++ MyProjects/ESP_To_Python/SourceCodes/WiFi_to_GET/WiFi_to_GET.ino
@@ -1,82 +1,31 @@
+/*
+  Tech01 simple python WiFi signal logger By Serhii Trush with MIT License.
+  https://github.com/techn0man1ac/WIFIDataLogger
+  Thank's ChatGPT for help.
+  By Tech01 labs 2024.
+*/
+
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <ArduinoJson.h> // Додати бібліотеку ArduinoJson
+#include <ArduinoJson.h> // ArduinoJson-7.0.4
 
-#ifndef STASSID
-#define STASSID "YOUSSID"
-#define STAPSK "YOUPASSWORD"
-#endif
-
-const char *ssid = STASSID;
-const char *password = STAPSK;
+const char* ssid = "YOUSSID";
+const char* password = "YOUPASSWORD";
 
 ESP8266WebServer server(80);
 
-const int led = 2;
-
-void handleRoot() {
-  digitalWrite(led, 1);
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  StreamString temp;
-  temp.reserve(500);  // Preallocate a large chunk to avoid memory fragmentation
-  temp.printf("\
-<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-              hr, min % 60, sec % 60);
-  server.send(200, "text/html", temp.c_str());
-  digitalWrite(led, 0);
-}
-
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
-}
-
-void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 1);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, 1); // LED off
   Serial.begin(115200);
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-  WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
+  // Connect to Wi-Fi network
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
 
   Serial.println("");
@@ -85,42 +34,38 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", handleRoot);
-  server.on("/inline", []() {
-    digitalWrite(led, 1);
-    
-    // Створення динамічного буфера для JSON
-    DynamicJsonDocument jsonBuffer(512); // Задайте розмір відповідно до вашої потреби
-
-    // Створення масиву JSON для зберігання інформації про мережі Wi-Fi
-    JsonArray networks = jsonBuffer.createNestedArray("networks");
-
-    // Отримання списку мереж Wi-Fi
-    int numNetworks = WiFi.scanNetworks();
-
-    // Додавання інформації про кожну мережу до масиву JSON
-    for (int i = 0; i < numNetworks; i++) {
-        JsonObject network = networks.createNestedObject();
-        network["ssid"] = WiFi.SSID(i);
-        network["signal_strength"] = WiFi.RSSI(i);
-    }
-
-    // Відправка даних JSON
-    String response;
-    serializeJson(jsonBuffer, response);
-    server.send(200, "application/json", response);
-     digitalWrite(led, 0);
-  });
-  server.onNotFound(handleNotFound);
+  // Start the web server
+  server.on("/devices", HTTP_GET, handleDevices);
   server.begin();
+
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+void loop() {
   server.handleClient();
-  MDNS.update();
+  digitalWrite(LED_BUILTIN, 1); // LED off
+}
+
+void handleDevices() {
+
+  // Create a dynamic JSON buffer
+  DynamicJsonDocument jsonBuffer(1024);
+
+  // Create a JSON array to store Wi-Fi network information
+  JsonArray devices = jsonBuffer.createNestedArray("devices");
+
+  // Scan Wi-Fi networks
+  int numNetworks = WiFi.scanNetworks();
+
+  // Add information about each device to the JSON array
+  for (int i = 0; i < numNetworks; i++) {
+    JsonObject device = devices.createNestedObject();
+    device["ssid"] = WiFi.SSID(i);
+    device["rssi"] = WiFi.RSSI(i);
+  }
+  digitalWrite(LED_BUILTIN, 0); // LED on
+  // Send data in JSON format
+  String response;
+  serializeJson(jsonBuffer, response);
+  server.send(200, "application/json", response);
 }
