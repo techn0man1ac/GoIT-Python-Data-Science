25c4f1f8ff63f79ba2a3d7029509d52317da56be
Techn0man1ac <raznie.podelki@gmail.com>
2024-04-29 10:08:45 +0300

    Change#068 big updates

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,11 @@
-print('Hello')
+from datetime import datetime
+import pytz
+
+# Встановлення місцевого часового поясу
+local_tz = pytz.timezone('Europe/Kiev')
+
+# Отримання поточного часу у місцевому часовому поясі
+current_time = datetime.now(local_tz)
+
+# Перевірка поточного часу
+print(current_time)
